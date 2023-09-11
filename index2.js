7acf3779a5fb1d1194291dacd17ca863f67597e3
Anuradha2k21 <61109105+Anuradha2k21@users.noreply.github.com>
2023-09-11 22:51:06 +0530

    variables

---
+++ index2.js
@@ -0,0 +1,7 @@
+var myName="Anuradha"   //  normal variable
+
+myName=44   //  can be redeclared into a different data type
+
+let myName="Sanjaya"    //  variable that is only available within the scope
+
+const ourName="Adikari"     //  value cannot be changed
